0887b77d4d4b9d872f418581c46dec91dbf9b722
CapitanFernet <gremiger@hotmail.es>
2017-12-12 17:16:14 -0300

    Delete solEj5.sql

--- Base de datos 111k/Soluciones/solEj5.sql
+++
@@ -1,4 +0,0 @@
-/* EJ5 Proponer consultas utilizando select, union, resta (not in), natural join */
-select * from alumno natural join realiza;
-select alumno.dni, nombre_y_apellido from alumno union select realiza.dni, realiza.nombre_curso from realiza;
-select * from alumno where alumno.dni not in (select realiza.dni from realiza)
